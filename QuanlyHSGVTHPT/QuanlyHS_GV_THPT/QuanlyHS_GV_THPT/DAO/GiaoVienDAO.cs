edd3fcdf8158fcf0f3d5defb92a0c4aa95340874
phandungmta <dungcnttk51mta@gmail.com>
2018-09-22 21:20:48 +0700

    add dao

---
+++ QuanlyHSGVTHPT/QuanlyHS_GV_THPT/QuanlyHS_GV_THPT/DAO/GiaoVienDAO.cs
@@ -0,0 +1,76 @@
+﻿using QuanlyHS_GV_THPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanlyHS_GV_THPT.DAO
+{
+    class GiaoVienDAO
+    {
+        private QuanlyHSGV db = new QuanlyHSGV();
+        public List<GIAOVIEN> ListGV(string searchString)
+        {
+            var listgv = db.GIAOVIENs.ToList();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                listgv = db.GIAOVIENs.Include("TOBM").Where(p => p.TENGV.ToUpper().Contains(searchString.ToUpper()) || p.TOBM.TENTO.ToUpper().Contains(searchString.ToUpper())).ToList();
+            }
+            return listgv;
+        }
+        public GIAOVIEN FindID(int id)
+        {
+            return db.GIAOVIENs.Find(id);
+        }
+        public bool Insert(GIAOVIEN gv)
+        {
+            try
+            {
+                db.GIAOVIENs.Add(gv);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Không thêm được");
+                return false;
+            }
+        }
+        public bool Edit(GIAOVIEN gv, int ID)
+        {
+            try
+            {
+                var GVEntry = db.GIAOVIENs.Find(ID);
+                GVEntry.TENGV = gv.TENGV;
+                GVEntry.NGAYSINH = gv.NGAYSINH;
+                GVEntry.SDT = gv.SDT;
+                GVEntry.DIACHI = gv.DIACHI;
+                GVEntry.TOBMID = gv.TOBMID;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Không sửa được");
+                return false;
+            }
+        }
+        public bool Delete(int ID)
+        {
+            try
+            {
+                var GV = db.GIAOVIENs.Find(ID);
+                db.GIAOVIENs.Remove(GV);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Bạn không thể xóa mục này!");
+                return false;
+            }
+        }
+    }
+}
---
+++ QuanlyHSGVTHPT/QuanlyHS_GV_THPT/QuanlyHS_GV_THPT/DAO/TKBDAO.cs
@@ -0,0 +1,73 @@
+﻿using QuanlyHS_GV_THPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanlyHS_GV_THPT.DAO
+{
+    class TKBDAO
+    {
+        private QuanlyHSGV db = new QuanlyHSGV();
+        public List<THOIKHOABIEU> ListTKB(string searchString)
+        {
+            var listTKB = db.THOIKHOABIEUx.ToList();
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                listTKB = db.THOIKHOABIEUx.Include("GIAOVIEN").Include("LOP").Where(p => p.GIAOVIEN.TENGV.ToUpper().Contains(searchString.ToUpper()) || p.LOP.TENLOP.ToUpper().Contains(searchString.ToUpper())).ToList();
+            }
+            return listTKB;
+        }
+        public THOIKHOABIEU FindID(int id)
+        {
+            return db.THOIKHOABIEUx.Find(id);
+        }
+        public bool Insert(THOIKHOABIEU tkb)
+        {
+            try
+            {
+                db.THOIKHOABIEUx.Add(tkb);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Không thêm được");
+                return false;
+            }
+        }
+        public bool Edit(THOIKHOABIEU tkb, int ID)
+        {
+            try
+            {
+                var TKBEntry = db.THOIKHOABIEUx.Find(ID);
+                TKBEntry.THU = tkb.THU;
+                TKBEntry.TIET = tkb.TIET;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Không sửa được");
+                return false;
+            }
+        }
+        public bool Delete(int ID)
+        {
+            try
+            {
+                var tkb = db.THOIKHOABIEUx.Find(ID);
+                db.THOIKHOABIEUx.Remove(tkb);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Bạn không thể xóa mục này!");
+                return false;
+            }
+        }
+    }
+}
