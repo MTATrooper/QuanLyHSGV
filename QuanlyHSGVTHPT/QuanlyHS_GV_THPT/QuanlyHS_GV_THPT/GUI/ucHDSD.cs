5fddfd9683662ada97e485515ea48f9989a71ad4
MTATrooper <long96nb@gmail.com>
2018-09-27 23:33:27 +0700

    final

---
+++ QuanlyHSGVTHPT/QuanlyHS_GV_THPT/QuanlyHS_GV_THPT/GUI/ucHDSD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanlyHS_GV_THPT.GUI
+{
+    public partial class ucHDSD : UserControl
+    {
+        public ucHDSD()
+        {
+            InitializeComponent();
+        }
+
+        private void pdfViewer1_Load(object sender, EventArgs e)
+        {
+             pdfViewer1.DocumentFilePath = @"43_huong_dan_QLHS_tieu_hoc.pdf";
+        }
+    }
+}
--- QuanlyHSGVTHPT/QuanlyHS_GV_THPT/QuanlyHS_GV_THPT/Program.cs
+++ QuanlyHSGVTHPT/QuanlyHS_GV_THPT/QuanlyHS_GV_THPT/Program.cs
@@ -10,7 +10,9 @@ namespace QuanlyHS_GV_THPT
 {
     static class Program
     {
-        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         [STAThread]
         static void Main()
         {
